77c61ea8c233559968e727a19c9b36dbeee0cd87
SergioVegas <sergio.vegas.7e8@itb.cat>
2024-12-30 11:58:15 +0100

    Comenzament ex

--- reviewexercises/Program.cs
+++ reviewexercises/Program.cs
@@ -2,6 +2,12 @@
 
  namespace reviewexercises
 {
+    public static void Main()
+    {
 
+    
+    //Constants
+    const string MsgSegons = "Introdueix un numero positiu i el passarem a hores, minuts i segons restants";
+    }
 
 }
