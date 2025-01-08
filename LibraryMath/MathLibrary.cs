370f9fea4ab7ba30143a612d6e74a8d28d056a77
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-01-08 13:05:45 +0100

    A seguir en casa

--- LibraryMath/MathLibrary.cs
+++ LibraryMath/MathLibrary.cs
@@ -3,10 +3,13 @@
     public class EX2
 
     {
-        public static int ClculateTriangleArea(int baseTriangle, int hight)
-        {
-            int area= baseTriangle * hight;
-
+        public static int ClculateTriangleArea(int baseTriangle, int hight, int area)
+        {   
+            if (baseTriangle < 0 || hight < 0) { throw new Exception("No pots introduir nombres negatius!"); }
+            else
+            {
+                 area = baseTriangle * hight;
+            }
             return area;
         }
     }
