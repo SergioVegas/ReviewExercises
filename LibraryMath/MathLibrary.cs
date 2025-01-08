320016ce16ca4da46ffd8c67986900fe51cb91f4
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-01-08 13:00:51 +0100

    Empezado

--- LibraryMath/MathLibrary.cs
+++ LibraryMath/MathLibrary.cs
@@ -1,7 +1,13 @@
 ﻿namespace LibraryMath
 {
-    public class Class1
-    {
+    public class EX2
 
+    {
+        public static int ClculateTriangleArea(int baseTriangle, int hight)
+        {
+            int area= baseTriangle * hight;
+
+            return area;
+        }
     }
 }
