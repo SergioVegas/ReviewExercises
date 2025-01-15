1c3a8091dd7e227378ed9265d289c956348cd85e
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-01-15 16:27:29 +0100

    Exercici acabat. #5

---
+++ LibraryMath/EX5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public   class EX5
+    {
+        public static double CelsiusToKelvin(double celsius)
+        {
+            return celsius + 273.15 ;
+        }
+        public static double CheckType()
+        {
+            double needCheck = 0;
+            bool flag = true;
+
+            while (flag)
+            {
+                try
+                {
+                    needCheck = double.Parse(Console.ReadLine());
+                    flag = false;
+                }
+                catch (FormatException)
+                { Console.WriteLine("Format incorrecte"); }
+            }
+            return needCheck;
+        }
+    }
+}
--- reviewexercises/Program.cs
+++ reviewexercises/Program.cs
@@ -2,6 +2,19 @@
 using MyLibrary;
  namespace reviewexercises
 {
+    public class EX3
+    {
+        public class Exercici3
+        {
+            public static void Main()
+            {
+                const string MsgEX3 = "Introdueix els graus Celsius";
 
+                Console.WriteLine(MsgEX3);
+                double celsius = EX5.CheckType();
+                Console.WriteLine($"{EX5.CelsiusToKelvin(celsius)} graus Fahrenheit");
+            }
+        }
+    }
 
 }
