e4ba1a9cab6eac98f74f63e312d8b5d4c46e5908
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-01-19 18:50:10 +0100

    Exercici 6 fet. #12

---
+++ LibraryMath/EX6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class EX6
+    {
+        public static double CheckType()
+        {
+            const string MsgNoNegative = "Aquest nombre no pot ser negatiu. ";
+            double needCheck = 0;
+            bool flag = true;
+
+            while (flag)
+            {
+                try
+                {
+                    needCheck = double.Parse(Console.ReadLine());
+                    flag = false;
+                    if (needCheck < 0)
+                    {
+                        flag = true;
+                        Console.WriteLine(MsgNoNegative);
+                    }
+                }
+                catch (FormatException)
+                { Console.WriteLine("Format incorrecte"); }
+
+            }
+            return needCheck;
+        }
+        public static double CheckDiscount(double initialPrice, double finalPrice)
+        {
+            return Math.Abs((finalPrice*100)/initialPrice-100);
+        }
+    }
+}
--- reviewexercises/Program.cs
+++ reviewexercises/Program.cs
@@ -2,6 +2,18 @@
 using MyLibrary;
  namespace reviewexercises
 {
+    public class Exercici6
+    {
+        public static void Main()
+        {
+            const string MsgEX6 = "Introdueix primer el preu inicial i després el preu final.";
 
+            Console.WriteLine(MsgEX6);
+            double initialPrice = EX6.CheckType();
+            double finalPrice = EX6.CheckType();
+            Console.WriteLine($"{EX6.CheckDiscount(initialPrice, finalPrice)}% es el descompte en aquest producte");
+
+        }
+    }
 
 }
