7626add00ba51b6c5772e5336e3fc6f470a1e25b
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-01-19 19:41:05 +0100

    Exercici més o menys acabat, els nombres molt grans no els dona be

---
+++ LibraryMath/EX7.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class EX7
+    {
+        public static int CheckType()
+        {
+            const string MsgNoNegative = "Aquest nombre no pot ser negatiu. ";
+            int needCheck = 0;
+            bool flag = true;
+
+            while (flag)
+            {
+                try
+                {
+                    needCheck = int.Parse(Console.ReadLine());
+                    flag = false;
+                    if (needCheck < 0)
+                    {
+                        flag = true;
+                        Console.WriteLine(MsgNoNegative);
+                    }
+                }
+                catch (FormatException)
+                { Console.WriteLine("Format incorrecte"); }
+
+            }
+            return needCheck;
+        }
+        public static void DecimalToHexadecimal(int decimalNumb)
+        {
+            int result = 0;
+
+
+            while (decimalNumb > 16)
+            {
+                result = decimalNumb / 16;
+                decimalNumb = decimalNumb % 16;
+
+                Console.Write(HexadecimalsLetters(result));
+            }
+            Console.WriteLine(HexadecimalsLetters(decimalNumb));
+        }
+        public static string HexadecimalsLetters(int hexadecimalNumb)
+        {
+            switch (hexadecimalNumb)
+            {
+                case 10:
+                    return "A";
+                    break;
+                case 11:
+                    return "B";
+                    break;
+                case 12:
+                    return "C";
+                    break;
+                case 13:
+                    return "D";
+                    break;
+                case 14:
+                    return "E";
+                    break;
+                case 15:
+                    return "F";
+                    break;
+                default:
+                    return $"{hexadecimalNumb}";
+            }
+        }
+    }
+}
--- reviewexercises/Program.cs
+++ reviewexercises/Program.cs
@@ -3,5 +3,16 @@ using MyLibrary;
  namespace reviewexercises
 {
 
+    public class Exercici7
+    {
+        public static void Main()
+        {
+            const string MsgEX7 = "Introdueix un numero en decimal per passar-ho a hexadecimal";
 
+            Console.WriteLine(MsgEX7);
+            int decimalNumb = EX7.CheckType();
+            EX7.DecimalToHexadecimal(decimalNumb);
+
+        }
+    }
 }
